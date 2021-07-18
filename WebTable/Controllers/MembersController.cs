2bf96c05f14e6df2eed6b2ec5de59fcaffa5a9d7
Emil <limezakiev@mail.ru>
2021-07-18 15:46:18 +0300

    Created controller MembersController.cs and method Get inside

---
+++ WebTable/Controllers/MembersController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace WebTable.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MembersController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+        public MembersController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            using (web_clientContext db = new web_clientContext())
+            {
+                var members = from member in db.Members
+                              select new {id = member.Id, name = member.Name, sername = member.Sername,
+                              middle_name = member.MiddleName, nickname = member.Nickname, email = member.Email,
+                              registration_date = member.RegistrationDate, last_active_date = member.LastActivityDate};
+                return (JsonResult)members;
+            }
+        }
+    }
+}
